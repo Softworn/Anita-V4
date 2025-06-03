0333aaf77fbec40a174de98da4bb7cdd31a6350f
Softworn <154608646+Softworn@users.noreply.github.com>
2025-06-03 12:07:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,41 +5,41 @@ require('dotenv').config();
 const toBool = (value) => value === "true";
 
 // Owner & Bot Details
-global.owner = process.env.OWNER_NUMBER;
+global.owner = process.env.OWNER_NUMBER;2348168821810
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.ownername = process.env.OWNER_NAME || "Onyemetu Michael";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
 // Auto-Features
-global.autoTyping = toBool(process.env.AUTO_TYPING);
-global.autoRecord = toBool(process.env.AUTO_RECORD);
-global.autoViewStatus = toBool(process.env.AUTO_VIEW_STATUS);
-global.AUTO_STATUS_REACT = toBool(process.env.AUTO_STATUS_REACT);
-global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
+global.autoTyping = toBool(process.env.AUTO_TYPING);true
+global.autoRecord = toBool(process.env.AUTO_RECORD);true
+global.autoViewStatus = toBool(process.env.AUTO_VIEW_STATUS);true
+global.AUTO_STATUS_REACT = toBool(process.env.AUTO_STATUS_REACT);true
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";true
 global.LEVELUP = toBool(process.env.LEVELUP);
-global.ANTIVIEWONCE = toBool(process.env.ANTIVIEWONCE);
+global.ANTIVIEWONCE = toBool(process.env.ANTIVIEWONCE);true
 
 global.public = process.env.PUBLIC === 'true';
 // Group & Security Settings
 
-global.ANTIDELETE = toBool(process.env.ANTIDELETE);
+global.ANTIDELETE = toBool(process.env.ANTIDELETE);true
 global.unavailable = toBool(process.env.UNAVAILABLE);
 global.available = toBool(process.env.AVAILABLE);
-global.autoreadmessages = toBool(process.env.AUTO_READ_MESSAGES);
-global.chatbot = toBool(process.env.CHATBOT);
-global.autoreact = toBool(process.env.AUTO_REACT);
-global.WELCOME = toBool(process.env.WELCOME);
+global.autoreadmessages = toBool(process.env.AUTO_READ_MESSAGES);true
+global.chatbot = toBool(process.env.CHATBOT);true
+global.autoreact = toBool(process.env.AUTO_REACT);true
+global.WELCOME = toBool(process.env.WELCOME);true
 
 // Prefix & Other Settings
 global.prefix = process.env.PREFIX || '.';
-global.autobio = toBool(process.env.AUTO_BIO);
-global.ANTI_TEMU = toBool(process.env.ANTI_TEMU);
-global.ANTI_TAG = toBool(process.env.ANTI_TAG);
+global.autobio = toBool(process.env.AUTO_BIO);true
+global.ANTI_TEMU = toBool(process.env.ANTI_TEMU);true
+global.ANTI_TAG = toBool(process.env.ANTI_TAG);true
 global.ANTICALL = toBool(process.env.ANTICALL);
-global.antilink = toBool(process.env.ANTILINK);
+global.antilink = toBool(process.env.ANTILINK);true
 global.antilinkkick = toBool(process.env.ANTILINK_KICK);
 global.antilinkwarn = toBool(process.env.ANTILINK_WARN);
 
